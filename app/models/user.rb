3683ada81bacc57f693a7ca779558ab7d7ec3d01
alexv7 <alexvarjabedian7@gmail.com>
2016-09-22 18:14:05 -0400

    changes to environment variables and user.rb to get aws to work

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" } #, default_url: "/images/:style/missing.png"
-  validates_attachment :image, content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }, size: { in: 0..3000.kilobytes }, presence: true
+  validates_attachment :image, content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }, size: { :less_than=>2.megabytes }#, presence: true
 end
