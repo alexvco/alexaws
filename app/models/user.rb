2ab29fbc2baa7a8c67e453544a7e7512dd3b3db8
alexv7 <alexvarjabedian7@gmail.com>
2016-09-22 02:43:26 -0400

    added validations to image

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" } #, default_url: "/images/:style/missing.png"
+  validates_attachment :image, content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }, size: { in: 0..3000.kilobytes }, presence: true
 end
